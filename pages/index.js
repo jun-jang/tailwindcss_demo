30fee6bf01e96795f5cc21f7b4880701029986d3
jun-jang <jun.jang@gmail.com>
2023-04-18 17:02:00 +0900

    32. Create all section for this page

--- pages/index.js
+++ pages/index.js
@@ -58,26 +58,129 @@ export default function Home() {
 							Menu
 						</a>
 					</div>
-				</header>
+        </header>
+        {/* Model S Section Start */}
 				<section id="modelS" class="car_section">
 					<img
 						src="./images/ModelS.jfif"
 						alt="Model S Image"
 						className="car_img"
 					></img>
-				</section>
-				<div className="car_details transform_50">
-					<h1 class="model_name"> Model S</h1>
-					<h2 class="tag_line"> Order Online for <span class="underline cursor-pointer">Touchles Delivery</span></h2>
-        </div>
-        <div class="car_buttons transform_50">
-          <button className="btn">Custom Order</button>
-          <button className="btn inventory">Existing Inventory</button>
-        </div>
+					<div className="car_details transform_50">
+						<h1 class="model_name"> Model S</h1>
+						<h2 class="tag_line">
+							{" "}
+							Order Online for{" "}
+							<span class="underline cursor-pointer">
+								Touchles Delivery
+							</span>
+						</h2>
+					</div>
+					<div class="car_buttons transform_50">
+						<button className="btn">Custom Order</button>
+						<button className="btn inventory">Existing Inventory</button>
+					</div>
 
-        <a href="#model3">
-          <svg className = "down_svg" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg"><path stroke="var(--tds-icon--fill, #171a20)" stroke-width="1.5" d="m19.5 12.5-4.5 4-4.5-4" fill="none" stroke-linecap="round" stroke-linejoin="round"/></svg>
-        </a>
+					<a href="#model3">
+						<svg
+							className="down_svg"
+							viewBox="0 0 30 30"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								stroke="var(--tds-icon--fill, #171a20)"
+								stroke-width="1.5"
+								d="m19.5 12.5-4.5 4-4.5-4"
+								fill="none"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+					</a>
+				</section>
+        {/* Model S Section End */}
+
+        {/* Model 3 Section Start */}
+				<section id="model3" class="car_section">
+					<img
+						src="./images/Model3.jfif"
+						alt="Model 3 Image"
+						className="car_img"
+					></img>
+					<div className="car_details transform_50">
+						<h1 class="model_name"> Model 3</h1>
+						<h2 class="tag_line">
+							{" "}
+							Order Online for{" "}
+							<span class="underline cursor-pointer">
+								Touchles Delivery
+							</span>
+						</h2>
+					</div>
+					<div class="car_buttons transform_50">
+						<button className="btn">Custom Order</button>
+						<button className="btn inventory">Existing Inventory</button>
+					</div>
+
+					<a href="#modelX">
+						<svg
+							className="down_svg"
+							viewBox="0 0 30 30"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								stroke="var(--tds-icon--fill, #171a20)"
+								stroke-width="1.5"
+								d="m19.5 12.5-4.5 4-4.5-4"
+								fill="none"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+					</a>
+				</section>
+        {/* Model 3 Section End */}
+
+        {/* Model X Section Start */}
+				<section id="modelX" class="car_section">
+					<img
+						src="./images/ModelX.jfif"
+						alt="Model X Image"
+						className="car_img"
+					></img>
+					<div className="car_details transform_50">
+						<h1 class="model_name"> Model X</h1>
+						<h2 class="tag_line">
+							{" "}
+							Order Online for{" "}
+							<span class="underline cursor-pointer">
+								Touchles Delivery
+							</span>
+						</h2>
+					</div>
+					<div class="car_buttons transform_50">
+						<button className="btn">Custom Order</button>
+						<button className="btn inventory">Existing Inventory</button>
+					</div>
+
+					<a href="#modelX">
+						<svg
+							className="down_svg"
+							viewBox="0 0 30 30"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								stroke="var(--tds-icon--fill, #171a20)"
+								stroke-width="1.5"
+								d="m19.5 12.5-4.5 4-4.5-4"
+								fill="none"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+					</a>
+				</section>
+        {/* Model X Section End */}
 			</body>
 		</>
 	);
