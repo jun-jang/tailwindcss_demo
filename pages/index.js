e010cc338b772d80573e3170acd26738e49d771a
jun-jang <jun.jang@gmail.com>
2023-04-18 16:09:07 +0900

    27. Starting with the navbar

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,49 @@ export default function Home() {
 				></link>
 			</Head>
 			{/* <IntroTailwind /> */}
+
+			<body>
+				<header className="navbar">
+					{/* Logo */}
+					<img src="./images/tesla.svg" alt="tesla logo" className="h-6" />
+					{/* Nav Links */}
+					<div>
+						<a href="#modelS" className="nav_link">
+							Model S
+						</a>
+						<a href="#model3" className="nav_link">
+							Model 3
+						</a>
+						<a href="#modelX" className="nav_link">
+							Model X
+						</a>
+						<a href="#modelY" className="nav_link">
+							Model Y
+						</a>
+						<a href="#solarRoof" className="nav_link">
+							Solar Roof
+						</a>
+						<a href="#solarPanels" className="nav_link">
+							Solar Panels
+						</a>
+					</div>
+					{/* Ext Links */}
+					<div>
+						<a href="#shop" className="nav_link">
+							{" "}
+							Shop{" "}
+						</a>
+						<a href="#account" className="nav_link">
+							{" "}
+							Account
+						</a>
+						<a href="#menu" className="nav_link">
+							{" "}
+							Menu
+						</a>
+					</div>
+				</header>
+			</body>
 		</>
 	);
 }
